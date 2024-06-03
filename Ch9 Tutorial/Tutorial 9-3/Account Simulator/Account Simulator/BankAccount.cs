6536529509a106e494464d3e5131e6c7b1088d59
WangSinYU <a112221002@mail.shu.edu.tw>
2024-06-03 17:30:16 +0800

    113-06-03

---
+++ Ch9 Tutorial/Tutorial 9-3/Account Simulator/Account Simulator/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System. Windows.Forms;
+
+namespace Account_Simulator
+{
+    class BankAccount
+    {
+        private decimal balance;
+
+        public BankAccount (decimal startingBalance) //建構子
+        {
+            balance = startingBalance;
+        }
+        public decimal Balance  //屬性
+        {
+            get { return balance; }
+        }
+
+        public void Deposit (decimal amount)
+        {
+            if (amount > 0m )
+            {
+                balance += amount;
+            }
+            else
+            {
+                MessageBox.Show("存款金額錯誤");
+            }
+        }
+        public void Withdraw (decimal amount)
+        {
+            if(amount > 0m && amount <= balance)
+            {
+                balance -= amount;
+            }
+            else
+            {
+                MessageBox.Show("存款金額額滿");
+            }
+        }
+    }
+}
---
+++ Ch9 Tutorial/Tutorial 9-4/Cell Phone Inventory/Cell Phone Inventory/CellPhone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cell_Phone_Inventory
+{
+    class CellPhone
+    {
+        // Fields
+        private string _brand;   // The phone's brand
+        private string _model;   // The phone's model
+        private decimal _price;  // Retail price
+
+        // Constructor
+
+        public CellPhone()
+        {
+            _brand = "";
+            _model = "";
+            _price = "";
+        }
+
+
+        // Brand property
+        public string Brand
+        {
+            get;
+            set;
+        }
+
+        public string Model
+        {
+            get;
+            set;
+        }
+     public decimal Price
+        {
+            get;
+            set;
+        }
+
+
+
+
+
+        // Model property
+
+
+
+
+        // Price property
+
+
+    }
+}
